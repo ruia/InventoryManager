ff3960fa77410328463fe776927989cd60bb8583
Rui Almeida <rui.almeida011@gmail.com>
2023-01-02 12:11:51 +0000

    Basic Product scaffolding

---
+++ database/migrations/2023_01_02_115556_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('ProductCode')->primary();
+            $table->string('ProductDescription');
+            $table->string('ProductNumberCode');
+            $table->string('ProductCategory')->default('M');
+            $table->string('UnitOfMeasure')->default('Unidade');
+            $table->decimal('PriceCost', 10,4);
+            // $table->decimal('PriceSale', 10,4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
